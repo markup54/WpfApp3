d3f9166dcf87e376eca74591999b0eefa7dcfd47
markup54 <marzenakupka@tlen.pl>
2024-03-13 15:17:24 +0100

    Dodaj pliki projektów.

---
+++ WpfApp3/Pytanie.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp3
+{
+    public class Pytanie
+    {
+        public string Tresc { get; set; }
+        public bool Odpowiedz { get; set; }
+
+        public Pytanie(string tresc, bool odpowiedz)
+        {
+            Tresc = tresc;
+            Odpowiedz = odpowiedz;
+        }
+    }
+}
---
+++ WpfApp3/WykonajTestWindow.xaml.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WpfApp3
+{
+    /// <summary>
+    /// Logika interakcji dla klasy WykonajTestWindow.xaml
+    /// </summary>
+    public partial class WykonajTestWindow : Window
+    {
+        List<Pytanie> PytanieList {get;set;} = new List<Pytanie>();
+        public WykonajTestWindow()
+        {
+            InitializeComponent();
+            przygotujPytania();
+            wyswietlPytanie(0);
+        }
+
+        private void wyswietlPytanie(int indeks)
+        {
+            trescPytanie_textblock.Text = PytanieList[indeks].Tresc;
+        }
+        private void przygotujPytania()
+        {
+            PytanieList.Clear();
+            StreamReader streamReader = new StreamReader("pytania.txt");
+            string trescpytania = streamReader.ReadLine();
+            string odpowiedz= streamReader.ReadLine();
+            while (trescpytania != null)
+            {
+                if (odpowiedz == "tak")
+                    PytanieList.Add(new Pytanie(trescpytania, true));
+                else
+                    PytanieList.Add(new Pytanie(trescpytania,false));
+                trescpytania = streamReader.ReadLine();
+                odpowiedz = streamReader.ReadLine();
+            }
+
+            streamReader.Close();
+        }
+
+        private void tak_button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void nie_button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
